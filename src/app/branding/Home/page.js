96a683e10518216475bb31e3df3282e20619cfbf
Ibrahim Hendi Zulkarnaen Nasution <ibra.nasution0307@gmail.com>
2024-06-19 16:08:13 +0700

    Tes

--- src/app/branding/Home/page.js
+++ src/app/branding/Home/page.js
@@ -1,16 +1,7 @@
-import { 
-  VStack,
-  SimpleGrid,
-  Heading,
-} from "@chakra-ui/react";
 import React from "react";
 
 export default function Home() {
   return(
-    <>
-      <VStack
-        minH="100vh"
-      ></VStack>
-    </>
+    <p>B</p>
   )
 }
